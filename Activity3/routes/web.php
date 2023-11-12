a14c003dbfe008e2a460a76111d1f184e105f9eb
Jp Mendoza <jeanpauljr.mendoza@gmail.com>
2023-11-12 09:59:21 +0800

    Updated Folder Names

--- Activity3/routes/web.php
+++ Activity3/routes/web.php
@@ -14,21 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('nav');
-})->name('nav');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
-
-Route::get('/welcome', function () {
     return view('welcome');
 });
---
+++ Activity4/resources/views/dashboard.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Activity5</title>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap/bootstrap.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="centered-container">
+        <div class="bordered">
+            <h1>THIS IS A TEMPORARY DASHBOARD</h1>
+        </div>
+    </div>
+</body>
+
+</html>
